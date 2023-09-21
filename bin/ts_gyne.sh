a3f25683a9fe8fad405db3f66246ea817ad670b4
Sandip Dutta <duttasandip11100@gmail.com>
2023-09-21 12:45:58 +0530

    gyne training script

--- bin/ts_gyne.sh
+++ bin/ts_gyne.sh
@@ -8,10 +8,13 @@ DATASET_ID=800
 
 # preprocess
 # python seg.py \
-#     --root_dir /data1/students/sandip/biomedical/code/data/raw/TS_Gyne \
+#     --root_dir /data1/students/sandip/biomedical/code/data/raw/TS_Gyne_2 \
 #     --dataset_id $DATASET_ID \
 #     --dataset_name TSGyne \
-#     --sample_start 0
+#     --sample_start 9
 
-# NNUnet preprocess
-nnUNetv2_plan_and_preprocess -d 800 --verify_dataset_integrity
+# # NNUnet preprocess
+# nnUNetv2_plan_and_preprocess -d 800 --verify_dataset_integrity
+
+# Train
+nnUNetv2_train $DATASET_ID 3d_fullres 0
