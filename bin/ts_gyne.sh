4e9b09ef6dceae497f6e08f0e6e1d532a1ba9c6e
Sandip Dutta <duttasandip11100@gmail.com>
2023-09-27 09:51:09 +0530

    Requirements

--- bin/ts_gyne.sh
+++ bin/ts_gyne.sh
@@ -8,14 +8,18 @@ DATASET_ID=800
 
 # preprocess
 # python seg.py \
+#     --root_dir /data1/students/sandip/biomedical/code/data/raw/TS_Gyne \
+#     --dataset_id $DATASET_ID \
+#     --dataset_name TSGyne
+
+# python seg.py \
 #     --root_dir /data1/students/sandip/biomedical/code/data/raw/TS_Gyne_2 \
 #     --dataset_id $DATASET_ID \
 #     --dataset_name TSGyne \
 #     --sample_start 9
 
 # # NNUnet preprocess
-# nnUNetv2_plan_and_preprocess -d 800 --verify_dataset_integrity
+# nnUNetv2_plan_and_preprocess -d $DATASET_ID --verify_dataset_integrity
 
 # Train
-nnUNetv2_train $DATASET_ID 3d_fullres 0 \
-    -tr nnUNetTrainerDiceCELoss_noSmooth
+nnUNetv2_train $DATASET_ID 3d_fullres 0 --c
