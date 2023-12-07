fa94d1205b639a77ec2d8a833539c77d00429c37
Sougata Maity <sougata.maity@tmckolkata.com>
2023-12-07 13:18:05 +0530

    start script

--- a9t/config.py
+++ a9t/config.py
@@ -9,7 +9,7 @@ DB_CONFIG = {
 }
 
 MODEL_CONFIG = {"KEYS": list(ALL_SEG_MAP.keys())}
-PRED_BATCH_SIZE = 4
+PRED_BATCH_SIZE = 1
 DICOM_WATCH_DIR = os.path.normpath("D:\DICOM Database\DICOM IMPORT\dicom")
 PROTOCOL_TO_MODEL = {
     "prostate": "TSPrime",
---
+++ start.sh
@@ -0,0 +1,4 @@
+# rm -rf data/db1
+CALL conda.bat activate
+conda activate a9t
+python ./run.py
