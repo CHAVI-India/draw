56e1d3f9cf3870909e5f867fce033a9d7f0131e3
Sandip Dutta <duttasandip11100@gmail.com>
2024-01-03 15:21:14 +0530

    Minor changes to config

--- draw/dao/table.py
+++ draw/dao/table.py
@@ -1,13 +1,18 @@
-import datetime
-
 from sqlalchemy import String, Column, Enum, DateTime, BigInteger
 from draw.dao.common import Base, Model, Status
 from sqlalchemy.sql import func
+from sqlalchemy.dialects import postgresql, mysql, sqlite
+
+
+BigIntegerType = BigInteger()
+BigIntegerType = BigIntegerType.with_variant(postgresql.BIGINT(), "postgresql")
+BigIntegerType = BigIntegerType.with_variant(mysql.BIGINT(), "mysql")
+BigIntegerType = BigIntegerType.with_variant(sqlite.INTEGER(), "sqlite")
 
 
 class DicomLog(Base):
     __tablename__ = "dicomlog"
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    id = Column(BigIntegerType, primary_key=True, autoincrement=True)
     series_name = Column("series_name", String(256))
     input_path = Column("input_path", String(1024))
     output_path = Column("output_path", String(1024))
@@ -21,7 +26,7 @@ class DicomLog(Base):
     created_on = Column(
         "created_on",
         DateTime,
-        server_default=func.now(),
+        server_default=func.current_timestamp(),
     )
 
     def __repr__(self):
